0e7e734533ede5240d8604f2af3b0fe6ca99f39b
haja <h.jagenteufel.dev@gmail.com>
2023-09-30 21:13:02 +0200

    remove some LSP mappings, add scala

--- init.lua
+++ init.lua
@@ -313,7 +313,7 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'lua', 'python', 'rust', 'vimdoc', 'vim', 'scala' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
@@ -536,6 +536,9 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
+-- custom configs
+vim.cmd.colorscheme("desert")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
