31771c351c1588a3e73977e95d4c76ea64c9a429
haja <h.jagenteufel.dev@gmail.com>
2023-09-30 21:12:16 +0200

    remove onedark theme, use icons in statusbar

--- init.lua
+++ init.lua
@@ -143,22 +143,12 @@ require('lazy').setup({
   },
 
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
-  },
-
-  {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
     opts = {
       options = {
-        icons_enabled = false,
-        theme = 'onedark',
+        icons_enabled = true,
         component_separators = '|',
         section_separators = '',
       },
