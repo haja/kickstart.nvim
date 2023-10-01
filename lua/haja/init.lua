2ce27f702ef2ea57481318dfdd2c9754c360213a
haja <h.jagenteufel.dev@gmail.com>
2023-10-01 11:58:40 +0200

    add neovide config

--- lua/haja/init.lua
+++ lua/haja/init.lua
@@ -3,6 +3,10 @@ require('haja.set')
 require('haja.command')
 local intellij_keymap = require('haja.intellij_keymap')
 
+if vim.g.neovide then
+    require('haja.neovide')
+end
+
 -- needed for kitty keyboard protocol / wezterm. seems unsupported vai tmux
 --vim.cmd([[autocmd UIEnter * if v:event.chan ==# 0 | call chansend(v:stderr, "\x1b[>1u") | endif]])
 --vim.cmd([[autocmd UILeave * if v:event.chan ==# 0 | call chansend(v:stderr, "\x1b[<1u") | endif]])
---
+++ lua/haja/neovide.lua
@@ -0,0 +1,13 @@
+
+vim.o.guifont = "Iosevka SS12 Extended:h10"
+
+--vim.g.neovide_transparency = 0.9
+vim.g.neovide_floating_blur_amount_x = 2.0
+vim.g.neovide_floating_blur_amount_y = 2.0
+
+-- does not work on wayland?
+--vim.g.neovide_remember_window_size = true
+
+vim.g.neovide_cursor_animation_length = 0
+vim.g.neovide_cursor_animate_in_insert_mode = false
+
